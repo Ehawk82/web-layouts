536d01103d4c9a471256e7ed176d63b1495b28f6
Jason Graziano <mrehawk82@gmail.com>
2019-02-09 17:38:05 -0500

    update for css and js

--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,26 @@ myUI = {
         myUI.loadout();
 	},
 	loadout: () => {
+		var tds = byTag("td");
+
         dvContain1.append("menu");
-		dvContain2.append("nav");
+        myUI.navToggleFunc(tds);
 		dvContain3.append("output");
+	},
+	navToggleFunc: (tds) => {
+		var navToggleBtn = createEle("button");
+
+        navToggleBtn.innerHTML = "\u2194";
+        navToggleBtn.className = "navToggleBtn";
+        navToggleBtn.onclick = myUI.doToggle(navToggleBtn, tds);
+
+        dvContain2.append(navToggleBtn);
+	},
+	doToggle: (navToggleBtn, tds) => {
+		return () => {
+console.log(tds);
+		}
+
 	}
 };
 
